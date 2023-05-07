c004139b6cdc90035d0e6f4af8d48278617a3e42
paulinau <paulinaugaldecarreno@gmail.com>
2023-05-07 00:03:38 -0600

    fixed desing

--- checkout.php
+++ checkout.php
@@ -103,7 +103,7 @@
                 </table>
                 <div class="row">
                     <div class="col-md-5 offset-md-7 d-grid gap-2">
-                        <button class="btn btn-primary btn-lg">Realizar pago</button>
+                        <button class="btn btn-primary btn-lg" id="pagar">Realizar pago</button>
                     </div>
                 </div>
             </div>
